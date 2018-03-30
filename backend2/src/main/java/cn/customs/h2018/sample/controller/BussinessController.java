0933cee39ec2b5762f49a8b7ba5c028e2eee73b8
lizhipeng <lizhipeng@gmail.com>
2018-03-30 12:05:11 +0800

    new

--- backend2/src/main/java/cn/customs/h2018/sample/controller/BussinessController.java
+++ backend2/src/main/java/cn/customs/h2018/sample/controller/BussinessController.java
@@ -6,7 +6,6 @@ package cn.customs.h2018.sample.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
-import lombok.extern.slf4j.Slf4j;
 
 
 /**  
@@ -16,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 @CrossOrigin(origins = "*", allowCredentials = "true", maxAge = 4800)
 @RestController
-@Slf4j
 public class BussinessController {
 
 }
